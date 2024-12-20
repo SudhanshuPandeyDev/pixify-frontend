f84986ea4600aac1d7a75a35888b4601d4974cf7
Sudhanshu Pandey <154343364+SudhanshuPandeyDev@users.noreply.github.com>
2024-12-20 12:39:20 +0530

    Update PhotoGallery.jsx file

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -119,7 +119,7 @@ const PhotoGallery = () => {
         <div className="text-center text-lg font-medium text-gray-600">
           <p>Loading photos, please wait...</p>
           {!isAuthenticated && (
-            <p classname="mt-4">
+            <p classname="mt-10">
               Please{" "}
               <span
                 className="text-blue-500 underline cursor-pointer"
